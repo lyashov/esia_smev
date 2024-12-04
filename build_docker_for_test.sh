da4568fd5c9f1389e6d22495947af7ac01539a62
evgeniiliashov <evgeniy.lyashov@rt.ru>
2024-12-04 15:40:04 +0300

    docker setting

--- build_docker_for_test.sh
+++ build_docker_for_test.sh
@@ -2,6 +2,6 @@ echo "\n***** BUILDING TEST JAR *****\n"
 mvn clean install -Ptest
 
 echo ""
-echo "\n***** BUILDING AND PUSHING DOCKER IMAGE TO 10.42.78.138 STORAGE *****\n"
+echo "\n***** BUILDING AND PUSHING DOCKER IMAGE TO STORAGE *****\n"
 echo ""
 mvn docker:build -DpushImageTag -Ptest
